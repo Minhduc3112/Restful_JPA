d1c664a8c371c110d2802b902858c77b340a8c73
WEBSITESHOPPING <tranthaomeii@gmail.com>
2021-07-24 18:51:50 +0700

    Initial commit

---
+++ src/main/java/com/example/restful_jpa/RestfulJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.restful_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restful_jpa/RestfulJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restful_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
